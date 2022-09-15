366a1d24e2aa23cd25fe47a93af626f62c220204
Monty-22 <sasha-cherba@yandex.ru>
2022-09-15 21:10:35 +0300

    Вариант прямого метода через сортировку

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,23 +8,10 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
-    cost_routers = [[0, 0]]
-    for i in range(len(stairway) - 1):
-        cost_1 = deepcopy(cost_routers)
-        cost_2 = deepcopy(cost_routers)
 
-        for j in range(len(cost_1) - 1):
-            cost_1[j][0] += 1
-            cost_1[j][1] += stairway[i]
-            cost_2[j][0] += 2
-            cost_2[j][1] += stairway[i + 1]
-        cost_routers = cost_1 + cost_2
-        print(f"{cost_routers}")
-    print(stairway)
-    print(cost_routers)
-    costes = []
-    for i in range(len(cost_routers) - 1):
-        costes.append(cost_routers[i][1])
-    x = min(costes)
-    print(x)
-    return x
+    stairway.sort()
+    cost = 0
+    for i in range(0, len(stairway), 2):
+        cost += stairway[i]
+    print(cost)
+    return cost
