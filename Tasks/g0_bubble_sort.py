2775af3a881cdf738e2fce57ad0e1d0bf1d66130
Monty-22 <sasha-cherba@yandex.ru>
2022-09-29 21:00:29 +0300

    Последний вариант

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -1,6 +1,13 @@
 from typing import List
 
 
+def swap (el_1, el_2):
+    temp = el_2
+    el_2 = el_1
+    el_1 = temp
+    print(temp)
+
+
 def sort(container: List[int]) -> List[int]:
     """
     Sort input container with bubble sort
@@ -8,4 +15,19 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+
+    while True:
+        count = 0
+        for i in range(len(container) - 1):
+            if container[i] > container[i + 1]:
+                swap(container[i], container[i + 1])
+                count += 1
+        if count == 0:
+            break
+
     return container
+
+if __name__ == "__main__":
+    list = [ 10, 17, 3, 58, 44, 100]
+    sort(list)
+    print(list)
